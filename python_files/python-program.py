f963fdb1e0314b206ffc040e8216edf020ba812d
vansh <vansh8174@gmail.com>
2026-07-03 09:53:34 +0530

    aloo

--- python_files/python-program.py
+++ python_files/python-program.py
@@ -1,6 +1,17 @@
-lst = [[3,44,32], [324,234,2]]
-
-print(lst[1,2])
+n = 7
+mid = (n//2) + 1
+for i in range(1,n+1):
+    for j in range(1, n+1):
+        if(i == mid or j == mid):
+            print('*',end=' ')
+        elif(i >= 2 and i <= n-1) and (j >= 2 and j <= n-1):
+            if(i == 2 or i == 6) and j != 4:
+                print('#', end=" ")
+            else:
+                print('*',end=' ')
+        else:
+            print(" ",end=" ")
+    print()
 
 
 
